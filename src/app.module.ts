1c1c3e3fd9563e68f1de83a967809504eab94476
Soulkaku <tangogitlab@gmail.com>
2025-06-05 16:21:43 -0300

    user removido será substituido por company

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [DatabaseModule, AuthModule],
 })
 export class AppModule {}
