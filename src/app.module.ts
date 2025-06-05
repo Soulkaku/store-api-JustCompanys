a42887f94ed1f6a1e001b147872ab8672cd58b2f
Soulkaku <tangogitlab@gmail.com>
2025-06-05 17:01:44 -0300

    adição do globalPipes e company module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { CompanyModule } from './company/company.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, CompanyModule],
+  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class AppModule {}
