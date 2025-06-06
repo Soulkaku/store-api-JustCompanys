84e27c1c3f581e21a9acfeab1a6dff4adcc2049d
Soulkaku <tangogitlab@gmail.com>
2025-06-06 15:49:52 -0300

    DTO criados para company

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCompanyDto {}
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateCompanyDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Length(3)
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Length(6)
+  @IsNotEmpty()
+  password: string;
+}
