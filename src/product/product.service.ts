98e91cba698ae761f8a0562975aa75ae9d603ac1
Soulkaku <tangogitlab@gmail.com>
2025-06-09 17:33:01 -0300

    feat: crud simples, necessita-se de revisão

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.sevice';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  @Inject()
+  private readonly prisma: PrismaService;
+
+  async create(data: CreateProductDto, companyId: number) {
+    const product = { ...data, company: { connect: { id: companyId } } };
+    return this.prisma.product.create({
+      data: product,
+    });
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async getAllProducts(where: Prisma.ProductWhereUniqueInput) {
+    return this.prisma.product.findMany({ where });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async updateProduct(params: {
+    where: Prisma.ProductWhereUniqueInput;
+    data: Prisma.ProductUpdateInput;
+  }) {
+    const { where, data } = params;
+
+    const updatePayload = { ...data };
+
+    return this.prisma.product.update({ where, data: updatePayload });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async delete(where: Prisma.ProductWhereUniqueInput) {
+    return this.prisma.product.delete({ where });
   }
 }
