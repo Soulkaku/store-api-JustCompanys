1400ce2469711314223bcdf553c5406d73092e32
Soulkaku <tangogitlab@gmail.com>
2025-06-09 15:29:06 -0300

    feat: adição do module de product

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { CompanyModule } from './company/company.module';
 import { APP_PIPE } from '@nestjs/core';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, CompanyModule],
+  imports: [DatabaseModule, AuthModule, CompanyModule, ProductModule],
   providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class AppModule {}
