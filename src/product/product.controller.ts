9269e6d1c0323aa6f0ba7b76b7a3ea15f7018879
Soulkaku <tangogitlab@gmail.com>
2025-06-09 17:31:02 -0300

    feat: create: primeira etapa do CRUD

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+  Body,
+  Controller,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { PrismaService } from 'src/database/prisma.sevice';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  @Inject()
+  private readonly productService: ProductService;
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
+  @UseGuards(AuthGuard)
+  create(@Body() createProductDto: CreateProductDto, @Request() req: any) {
+    const companyId: number = req.company.sub
+    // console.log(req.body);
+    // console.log(req.company);
 
-  @Get()
-  findAll() {
-    return this.productService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+    return this.productService.create(createProductDto, companyId);
   }
 }
