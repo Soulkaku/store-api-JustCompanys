698cae8941bc13482e1c00f7db321e9c8bc0f014
Soulkaku <tangogitlab@gmail.com>
2025-06-09 17:30:25 -0300

    fix: companyId agora é compreendida

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,9 @@
-export class Product {}
+import { Product } from '@prisma/client';
+
+export class IProduct implements Product {
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  companyId: number;
+}
