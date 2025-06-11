ce9a890ba24ca741d898b5d69456996c17e03769
Soulkaku <tangogitlab@gmail.com>
2025-06-10 21:54:54 -0300

    feat: função para atualizar somente um produto

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,15 +1,20 @@
 import {
   Body,
   Controller,
+  Get,
   Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
   Post,
   Request,
   UseGuards,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { PrismaService } from 'src/database/prisma.sevice';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('product')
 export class ProductController {
@@ -18,11 +23,34 @@ export class ProductController {
 
   @Post()
   @UseGuards(AuthGuard)
-  create(@Body() createProductDto: CreateProductDto, @Request() req: any) {
-    const companyId: number = req.company.sub
-    // console.log(req.body);
-    // console.log(req.company);
+  create(
+    @Body(new ValidationPipe()) createProductDto: CreateProductDto,
+    @Request() req: any,
+  ) {
+    const companyId: number = req.company.sub;
+
+    console.log(req.company);
 
     return this.productService.create(createProductDto, companyId);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard)
+  getAllProducts(@Request() req: any) {
+    const companyId: number = req.company.sub;
+
+    return this.productService.getAll(companyId);
+  }
+
+  @Patch('update/:id')
+  @UseGuards(AuthGuard)
+  updateProduct(
+    @Body(new ValidationPipe()) updateProductDto: UpdateProductDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.productService.updateOne({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 }
