5ad9090ee1e770b76bed17d33867a72155dc6a5e
Soulkaku <tangogitlab@gmail.com>
2025-06-11 15:36:09 -0300

    feat: swagger para documentação de rotas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API-COMPANYS AND PRODUCTS')
+    .setDescription(
+      'Api para estudar o nest e como relacionar tabelas SQL através do prisma',
+    )
+    .setVersion('1.0')
+    .addTag('Store')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
