608b6fa00a585f249d50aef5b64d34765d0e8077
Soulkaku <tangogitlab@gmail.com>
2025-06-11 16:24:25 -0300

    feat: decorators para informações que devem ser informadas a documentação

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateCompanyDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @Length(3)
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @Length(6)
   @IsNotEmpty()
   password: string;
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
 
+  @ApiPropertyOptional()
   description: string;
 }
